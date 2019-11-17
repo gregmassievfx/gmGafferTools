c31eaac5e64530bcf6edb1296090a5f6b8b32ddc
Greg Massie VFX <greg@gregmassie.com>
2019-11-16 17:35:45 -0800

    would like to append test to output but can't do it in the button actions

--- python/Toolbox/ToolboxUI/ToolboxUI.py
+++ python/Toolbox/ToolboxUI/ToolboxUI.py
@@ -8,7 +8,6 @@ from Qt import QtCore
 
 import Toolbox
 
-# that works in a specific namespace.
 class ToolboxUI(GafferUI.Editor):
 
     def __init__( self, scriptNode, **kw):
@@ -25,57 +24,56 @@ class ToolboxUI(GafferUI.Editor):
                 GafferUI.Label( "Some test buttons")
                 # create action buttons
                 self.__buttonTest1 = GafferUI.Button(text="my button1")
-                self.__buttonTest1.clickedSignal().connect( self.__button1Action )
+                self.__buttonTest1.clickedSignal().connect( self.__button1Action() )
 
                 self.__divider1 = GafferUI.Divider(GafferUI.Divider.Orientation.Horizontal)
 
                 self.__buttonTest2 = GafferUI.Button(text="my button2")
-                self.__buttonTest2.clickedSignal().connect( self.__button2Action )
+                self.__buttonTest2.clickedSignal().connect( self.__button2Action() )
 
         # create feedback text window#################################
-		with self.__splittable:
+        with self.__splittable:
 
-			self.__outputWidget = GafferUI.MultiLineTextWidget(
-				text="Toolbox output",
-				editable=False,
-				wrapMode=GafferUI.MultiLineTextWidget.WrapMode.None,
-				role=GafferUI.MultiLineTextWidget.Role.Code,
-			)
-			self.__outputWidget._qtWidget().setProperty("gafferTextRole", "output")
-
-
-
+            self.__outputWidget = GafferUI.MultiLineTextWidget(
+                text="Toolbox output",
+                editable=False,
+                wrapMode=GafferUI.MultiLineTextWidget.WrapMode.None,
+                role=GafferUI.MultiLineTextWidget.Role.Code,
+            )
+            self.__outputWidget._qtWidget().setProperty("gafferTextRole", "output")
+            self.__outputWidget.appendText(str("banana"))
 
     def outputWidget(self):
         return self.__outputWidget
 
     def __button1Action(self):
         print "yep, hit button 1"
-
+        Toolbox.test()
+        # self.__outputWidget.appendText( str("banana") )
 
     def __button2Action(self):
         print "yep, hit button 2"
-		# Toolbox.test()
+        Toolbox.test()
 
 
 GafferUI.Editor.registerType("Toolbox", ToolboxUI)
 
 
-class _MessageHandler(IECore.MessageHandler):
-
-    def __init__(self, textWidget):
-        IECore.MessageHandler.__init__(self)
-
-        self.__textWidget = textWidget
-
-    def handle(self, level, context, message):
-        html = formatted = "<h1 class='%s'>%s : %s </h1><pre class='message'>%s</pre><br>" % (
-            IECore.Msg.levelAsString(level),
-            IECore.Msg.levelAsString(level),
-            context,
-            message
-        )
-        self.__textWidget.appendHTML(html)
-        # update the gui so messages are output as they occur, rather than all getting queued
-        # up till the end.
-        QtWidgets.QApplication.instance().processEvents(QtCore.QEventLoop.ExcludeUserInputEvents)
+# class _MessageHandler(IECore.MessageHandler):
+#
+#     def __init__(self, textWidget):
+#         IECore.MessageHandler.__init__(self)
+#
+#         self.__textWidget = textWidget
+#
+#     def handle(self, level, context, message):
+#         html = formatted = "<h1 class='%s'>%s : %s </h1><pre class='message'>%s</pre><br>" % (
+#             IECore.Msg.levelAsString(level),
+#             IECore.Msg.levelAsString(level),
+#             context,
+#             message
+#         )
+#         self.__textWidget.appendHTML(html)
+#         # update the gui so messages are output as they occur, rather than all getting queued
+#         # up till the end.
+#         QtWidgets.QApplication.instance().processEvents(QtCore.QEventLoop.ExcludeUserInputEvents)
