6bbbf493d3073c43d98903a780276275715fa7f0
Greg Massie VFX <greg@gregmassie.com>
2019-11-23 19:14:08 -0800

    Working on making buttons that do things! Baby steps.

--- python/Toolbox/ToolboxUI/ToolboxUI.py
+++ python/Toolbox/ToolboxUI/ToolboxUI.py
@@ -3,9 +3,6 @@ import IECore
 import Gaffer
 import GafferUI
 
-from Qt import QtWidgets
-from Qt import QtCore
-
 import Toolbox
 
 class ToolboxUI(GafferUI.Editor):
@@ -16,64 +13,63 @@ class ToolboxUI(GafferUI.Editor):
         GafferUI.Editor.__init__( self, self.__splittable, scriptNode, **kw)
 
         with self.__splittable:
-            # create main area for action buttons
-            self.__toolboxContainer = GafferUI.ListContainer( spacing=5, borderWidth=5 )
+            # create splittable container so we can have actions tabs and a feedback window.
 
-            with self.__toolboxContainer:
+            with GafferUI.TabbedContainer( ) as self.__tab1Container:
+                # create tabbed container for action buttons
 
-                GafferUI.Label( "Some test buttons")
-                # create action buttons
-                self.__buttonTest1 = GafferUI.Button(text="my button1")
-                self.__buttonTest1.clickedSignal().connect( self.__button1Action() )
+                with GafferUI.ListContainer(spacing=5, borderWidth=5) as self.__toolboxContainer1:
+                    self.__tab1Container.setLabel(self.__toolboxContainer1, "test")
 
-                self.__divider1 = GafferUI.Divider(GafferUI.Divider.Orientation.Horizontal)
+                    GafferUI.Label( "Some test buttons")
+                    # create action buttons
+                    self.__buttonTest1 = GafferUI.Button(text="my button1")
+                    self.__buttonTest1.clickedSignal().connect( self.__button1Action, scoped=False)
 
-                self.__buttonTest2 = GafferUI.Button(text="my button2")
-                self.__buttonTest2.clickedSignal().connect( self.__button2Action() )
+                    self.__divider1 = GafferUI.Divider(GafferUI.Divider.Orientation.Horizontal)
+
+                    self.__buttonTest2 = GafferUI.Button(text="my button2")
+                    self.__buttonTest2.clickedSignal().connect( self.__button2Action, scoped=False )
+
+                with GafferUI.ListContainer(spacing=5, borderWidth=5) as self.__toolboxContainer2:
+                    self.__tab1Container.setLabel(self.__toolboxContainer2, "test2")
+
+                    GafferUI.Label("Some test buttons 2")
+                    # create action buttons
+                    self.__buttonTest3 = GafferUI.Button(text="my button1")
+                    self.__buttonTest3.clickedSignal().connect(self.__button1Action, scoped=False)
+
+                    self.__divider1 = GafferUI.Divider(GafferUI.Divider.Orientation.Horizontal)
+
+                    self.__buttonTest4 = GafferUI.Button(text="my button2")
+                    self.__buttonTest4.clickedSignal().connect(self.__button2Action, scoped=False)
+
 
         # create feedback text window#################################
         with self.__splittable:
 
-            self.__outputWidget = GafferUI.MultiLineTextWidget(
-                text="Toolbox output",
+            self.__feedbackWidget = GafferUI.MultiLineTextWidget(
+                text="Toolbox Feedback",
                 editable=False,
                 wrapMode=GafferUI.MultiLineTextWidget.WrapMode.None,
                 role=GafferUI.MultiLineTextWidget.Role.Code,
             )
-            self.__outputWidget._qtWidget().setProperty("gafferTextRole", "output")
-            self.__outputWidget.appendText(str("banana"))
+
+            self.__feedbackWidget._qtWidget().setProperty("gafferTextRole", "output")
+            # how on earth do I change the colour?!
+
 
     def outputWidget(self):
         return self.__outputWidget
 
-    def __button1Action(self):
-        print "yep, hit button 1"
-        Toolbox.test()
-        # self.__outputWidget.appendText( str("banana") )
+    def __button1Action(self, button):
+        Toolbox.testButton( "yep, hit button 1")
+        Toolbox.testFeedback(self.__feedbackWidget, "banana")
+        Toolbox.buildABox( self )
 
-    def __button2Action(self):
-        print "yep, hit button 2"
-        Toolbox.test()
+    def __button2Action(self, button):
+        Toolbox.testButton( "yep, hit button 2")
+        Toolbox.testFeedback( self.__feedbackWidget, "apple" )
 
 
 GafferUI.Editor.registerType("Toolbox", ToolboxUI)
-
-
-# class _MessageHandler(IECore.MessageHandler):
-#
-#     def __init__(self, textWidget):
-#         IECore.MessageHandler.__init__(self)
-#
-#         self.__textWidget = textWidget
-#
-#     def handle(self, level, context, message):
-#         html = formatted = "<h1 class='%s'>%s : %s </h1><pre class='message'>%s</pre><br>" % (
-#             IECore.Msg.levelAsString(level),
-#             IECore.Msg.levelAsString(level),
-#             context,
-#             message
-#         )
-#         self.__textWidget.appendHTML(html)
-#         # update the gui so messages are output as they occur, rather than all getting queued
-#         # up till the end.
-#         QtWidgets.QApplication.instance().processEvents(QtCore.QEventLoop.ExcludeUserInputEvents)
--- python/Toolbox/Tools/Tools.py
+++ python/Toolbox/Tools/Tools.py
@@ -1,2 +1,54 @@
-def test():
-    print "my test"
+import Gaffer
+import GafferUI
+import GafferScene
+import imath
+
+
+def testButton( output ):
+    print output
+
+def testFeedback( feedbackWidget, output):
+    feedbackWidget.appendText(str(output))
+
+
+def buildABox( source ):
+    # The box
+    root = GafferUI.Editor.scriptNode( source )
+
+    box = Gaffer.Box( "GroundPlane" )
+    Gaffer.Metadata.registerValue( box, 'icon', None )
+    root.addChild( box )
+
+    # Internal network
+
+    plane = GafferScene.Plane( "Plane" )
+    plane["transform"]["rotate"].setValue( imath.V3f( -90, 0, 0 ) )
+    plane["dimensions"].setValue( imath.V2f( 100, 100 ) )
+    plane["name"].setValue( "ground" )
+
+    freeze = GafferScene.FreezeTransform( "FreezeTransform" )
+
+    parent = GafferScene.Parent( "Parent" )
+    parent["parent"].setValue("/")
+
+    freeze["in"].setInput( plane["out"] )
+    parent["child"].setInput( freeze["out"] )
+
+    box.addChild( plane )
+    box.addChild( freeze )
+    box.addChild( parent )
+
+    # Promote i/o
+    boxInPlug = Gaffer.BoxIO.promote( parent["in"] )
+    boxOutPlug = Gaffer.BoxIO.promote( parent["out"] )
+
+    # Promote useful Node Editor plugs
+    Gaffer.BoxIO.promote( plane["name"] )
+    Gaffer.BoxIO.promote( parent["parent"] )
+    Gaffer.BoxIO.promote( plane["divisions"] )
+    Gaffer.BoxIO.promote( plane["dimensions"] )
+
+    # Add a passthrough
+    boxOutNode = boxOutPlug.getInput().node()
+    boxInNode = boxInPlug.outputs()[0].node()
+    boxOutNode["passThrough"].setInput( boxInNode["out"] )
